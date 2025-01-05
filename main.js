2ecc4bdb62f03ba203bb74dca98007d1516d8da7
Abuzaina-PS <abozene2004@gmail.com>
2025-01-05 20:15:13 +0200

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-function changeText(button) {
-  button.textContent = "Clicked!";
-}
+const button = document.getElementById("myButton");
+const input = document.getElementById("myInput");
+const result = document.getElementById("inputVal");
+
+// Change button text on mouse events
+button.addEventListener("mousedown", function () {
+  button.textContent = "Adding Task...";
+});
+
+button.addEventListener("mouseup", function () {
+  button.textContent = "Add New Task";
+});
+
+// Validate input on button click
+button.addEventListener("click", function () {
+  const inputValue = input.value.trim(); // Get the input value and remove extra spaces
+
+  if (inputValue == "") {
+    // Check if the input is empty
+    result.textContent = "Task can not be empety.";
+    result.style.color = "red";
+  } else if (inputValue.length < 3) {
+    // Check if the input is less than 3 characters
+    result.textContent = "task must be at least 5 charecters long";
+    result.style.color = "red";
+  } else if (/^\d/.test(inputValue)) {
+    // Check if the text starts with a number
+    result.textContent = "Task can not starts with a number.";
+    result.style.color = "red";
+  } else {
+  }
+});
